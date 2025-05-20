cc9b5b345c6f38dd57eab9b14c0a932a3c55a173
PanMinThai <146918562+PanMinThai@users.noreply.github.com>
2025-05-20 17:48:17 +0700

    3.1 Display data from Textbox to Messagebox, 3.2 Understand two-way binding

---
+++ Excercise3/Excercise3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Excercise3.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Excercise3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)// chuot trai co giu hay ko
+                DragMove();// keo window
+        }
+
+        private void btnMinisize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dữ liệu đây: "+ txtInput.Text);
+        }
+    }
+}
